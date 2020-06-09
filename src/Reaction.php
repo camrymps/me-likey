4f811ee0c363e666440ff8dd1811ecd07b96e242
Michael Scott <camrymps@gmail.com>
2020-06-09 14:14:08 -0400

    Append replaced and revoked attributes to model, making them visible in the resulting array when the model is serialized.

--- src/Reaction.php
+++ src/Reaction.php
@@ -17,10 +17,34 @@ class Reaction extends Model
     public $revoked = false;
 
     /**
+     * @var array
+     */
+    public $appends = [
+        'replaced',
+        'revoked'
+    ];
+
+    /**
      * Get all of the models that own reactions.
      */
     public function reactionable()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * Get the model's replaced attribute.
+     */
+    public function getReplacedAttribute()
+    {
+        return $this->replaced;
+    }
+
+    /**
+     * Get the model's revoked attribute.
+     */
+    public function getRevokedAttribute()
+    {
+        return $this->revoked;
     }
 }
